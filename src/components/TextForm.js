2c766702262303f251d59b0c8bd1a823b2d279eb
coder1311 <rituraj1311@gmail.com>
2023-07-04 09:33:38 +0530

    TextUtility commit-3

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,19 +7,45 @@ export default function TextForm(props) {
         setText(text.toUpperCase({text}))
     }
 
+    const handleLoClick = () =>{
+        setText(text.toLowerCase({text}))
+    }
+
+    const clearText = () =>{
+        setText('')
+    }
+
+    const handleTitleClick = () =>{
+        setText(text.split(' ')
+        .map(w => w[0].toUpperCase() + w.substring(1).toLowerCase())
+        .join(' ')
+        )
+    }
+
+
     const handleOnChange = (event) =>{
         setText(event.target.value)
     }
 
-    const [text, setText] = useState("Enter text:");
+    const [text, setText] = useState("");
 
-  return (
-    <div>
+    return (
+    <div className="container">
       <div className="mb-3">
         <h1>{props.heading}</h1>
         <textarea className="form-control" value={text} onChange={handleOnChange} id="textForm" rows="8" />
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+      <button className="btn btn-primary mx-1" onClick={clearText}>Clear Text</button>
+      <button className="btn btn-primary mx-1" onClick={handleTitleClick}>Convert to TitleCase</button>
+      <div className="container my-5">
+        <h2>Text Summary</h2>
+        <p>{text.split(" ").length} Words and {text.length} Characters</p>
+        <p>{0.008 * text.length} Minutes read</p>
+        <h2>Preview Text</h2>
+        <p>{text}</p>
+      </div>
     </div>
-  );
+    );
 }
