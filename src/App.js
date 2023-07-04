88dedc37bc633bfa0303c46ffd1c385bdc778433
coder1311 <rituraj1311@gmail.com>
2023-07-04 12:49:00 +0530

    TextUtility commit-4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtility" />
       <div className="container my-3">
-        <TextForm heading="Enter The Text Below"/>
+        {/* <TextForm heading="Enter The Text Below"/> */}
+        <About></About>
       </div>
       
     </>
