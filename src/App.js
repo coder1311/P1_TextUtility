39312b69b106d9cb8c012606682e2d3183d848ca
coder1311 <rituraj1311@gmail.com>
2023-07-16 13:35:11 +0530

    TextUtility commit-6

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert= (message, type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
 
   const toggleMode= ()=>{
     if(mode==='light'){
       setMode('dark')
       document.body.style.backgroundColor='#042743'
+      showAlert("Dark mode is Enabled", "success")
     }
     else{
       setMode('light')
       document.body.style.backgroundColor='white'
+      showAlert("Light mode is Enabled", "success")
     }
   }
   return (
     <>
       <Navbar title="TextUtility" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter The Text Below" mode={mode}/>
+        <TextForm heading="Enter The Text Below" mode={mode} showAlert={showAlert}/>
         {/* <About></About> */}
       </div>
       
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,14 +5,17 @@ export default function TextForm(props) {
 
     const handleUpClick = () =>{
         setText(text.toUpperCase({text}))
+        props.showAlert("Converted to UpperCase", "success");
     }
 
     const handleLoClick = () =>{
         setText(text.toLowerCase({text}))
+        props.showAlert("Converted to LowerCase", "success");
     }
 
     const clearText = () =>{
         setText('')
+        props.showAlert("Cleared Text", "success");
     }
 
     const handleTitleClick = () =>{
@@ -20,17 +23,21 @@ export default function TextForm(props) {
         .map(w => w[0].toUpperCase() + w.substring(1).toLowerCase())
         .join(' ')
         )
+        props.showAlert("Converted to TitleCase", "success");
     }
 
     const handleCopy = () => {
-      var text = document.getElementById("myBox");
+      let text = document.getElementById("myBox");
       text.select();
+      text.setSelectionRange(0, 9999);
       navigator.clipboard.writeText(text.value);
+      props.showAlert("Copied to Clipboard", "success");
     }
 
     const handleExtraSpaces = () => {
       let newText = text.split(/[ ]+/);
       setText(newText.join(" "))
+      props.showAlert("Extra Spaces Removed", "success");
     }
 
     const handleOnChange = (event) =>{
@@ -42,7 +49,7 @@ export default function TextForm(props) {
     return (
     <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
-        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="textForm" rows="8" />
+        <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} rows="8" />
       <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
       <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
       <button className="btn btn-primary mx-1" onClick={clearText}>Clear Text</button>
